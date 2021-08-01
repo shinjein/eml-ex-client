320dd38a6d6a5f0660e60781cbca09118214d737
shinjein <shinjein@hu-berlin.de>
2021-08-01 15:15:07 +0200

    multipart form

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -5,10 +5,10 @@ class Upload extends React.Component {
   state = {
 			fileNames: [],
       selectedFiles: null,
-			oneFile: null,
+			field_name: ''
 	};
 
-	setFileState = e => {
+	setFileData = e => {
 		const files = e.target.files;
 		let names = [];
 		for (let i = 0; i < files.length; i++) {
@@ -17,24 +17,33 @@ class Upload extends React.Component {
 		}
 	  this.setState({ 
 			fileNames: names,
-      selectedFiles: e.target.files,
-			oneFile: e.target.files[0].text
-    });
+      selectedFiles: files
+		});
+	};
+
+	setFieldName = e => {
+		const input = e.target.field_name;
+		let names = [];
+	  this.setState({ 
+      field_name: input
+		});
 	};
 
 	onFileUpload = async (e) => {
 		e.preventDefault();
-		const { selectedFiles } = this.state;
-		const data = new FormData();
-			data.append("files", selectedFiles);
+		const { field_name, selectedFiles } = this.state;
+		const formData = new FormData();
+			for (const file of selectedFiles ) {
+				formData.append(field_name, file);
+			}
 				const config = {
 					headers: {
 						'content-type': 'multipart/form-data'
 					}
 				}
-				console.log(data)
-		const response = await postfiles(data, config);
-			console.log(response);
+				console.log("files sent to server", formData)
+		const response = await postfiles(formData, config);
+		console.log(response.data);
 	}
 
 	renderFileData = () => {	
@@ -57,7 +66,8 @@ class Upload extends React.Component {
 					<br />
 					<h4>Instructions:</h4>
 					<ol>
-						<li>Select .eml files</li>
+						<li>Name bulk (date_lastname format: 210731_shin)</li>
+						<li>Select .eml files (max 10 files)</li>
 						<li>Click send to extract email information</li>
 					</ol>
 				</div>
@@ -68,12 +78,15 @@ class Upload extends React.Component {
 	render() {
 	return (
 		<div>
-			<h1>.EML XP</h1>
-			<h4>view contents of .eml files in bulk without opening them</h4>
+			<h1>Sm@il</h1> 
+			<h4>view contents of bulk .eml files without opening them</h4>
 				<form>
+					<label>Field Name</label>
+						<input type="text" name="field_name" id="field_name" 
+							multiple onChange={this.setFileData} /> 
 					<label htmlFor="file">Upload Files:</label>
-					<input type="file" name="file" id="file" 
-						multiple onChange={this.setFileState} /> 
+						<input type="file" name="file" id="file" 
+							multiple onChange={this.setFileData} /> 
 					<button onClick={this.onFileUpload}> Send </button> 
 				</form>
 			{this.renderFileData()}
